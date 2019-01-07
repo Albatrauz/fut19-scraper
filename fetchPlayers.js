67bbe45a87439bd7c3d8cea04448a21e693fd6fc
Michael van Gorkum <michael@nobears.nl>
2019-01-07 13:17:23 +0100

    Started implementation of progress bar

--- fetchPlayers.js
+++ fetchPlayers.js
@@ -1,5 +1,6 @@
 let axios = require('axios'); // HTTP client
 let fs = require('fs'); // is included in node.js - you don't need to install it
+let ProgressBar = require('progress');
 
 let totalPage;
 let currentPage = 1;
@@ -8,25 +9,33 @@ let players = [];
 function getPlayer() {
   axios.get('https://www.easports.com/fifa/ultimate-team/api/fut/item?page=' + currentPage)
     .then((response) => {
-       totalPage = response.data.totalPages;
-
+      totalPage = response.data.totalPages;
       if (currentPage < totalPage) {
         currentPage++;
         response.data.items.forEach(element => {
           players.push(element);
         });
         getPlayer();
+        
       }
       else {
         console.log(players.length);
+        console.log(totalPage);
         fs.writeFile('data/players.json',
           JSON.stringify(players, null, 3), (err) => {
             console.log('File successfully written!');
           })
       }
-      // console.log(response.data.currentPage);
 
     }, (error) => console.log(error));
 }
+let bar = new ProgressBar(':bar', { total: 726 });
+let timer = setInterval(function () {
+  bar.tick();
+  if (bar.complete) {
+    console.log('\ncomplete\n');
+    clearInterval(timer);
+  }
+}, 100);
 getPlayer();
 
